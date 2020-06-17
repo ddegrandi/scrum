e1cd69bc8b45a6c593613a9d2a022a1ae2575754
Daniele Roncolletta <daniele.roncolletta@gmail.com>
2020-06-17 20:55:09 -0300

    Group Complete

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,4 @@ Route::delete('/plans/{plan}', 'Api\\PlanController@destroy');
 */
 
 Route::resource('plans','api\\PlanController')->except(['create','edit']);
+Route::resource('groups','api\\GroupController')->except(['create','edit']);
